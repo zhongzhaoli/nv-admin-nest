961c4c4f930c7a54beda0f9ef9e4b171f12d4112
zhongzhaoli <525966315@qq.com>
2024-04-15 22:14:31 +0800

    feat: Department模块初始化

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionParams } from './config/typeorm.config';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
+import { DepartmentModule } from './department/department.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionParams), UserModule, RoleModule],
+  imports: [
+    TypeOrmModule.forRoot(connectionParams),
+    UserModule,
+    RoleModule,
+    DepartmentModule,
+  ],
   controllers: [],
   providers: [Logger],
   exports: [Logger],
---
+++ src/department/department.entity.ts
@@ -0,0 +1,35 @@
+import { Exclude } from 'class-transformer';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Department {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @IsNotEmpty()
+  @Length(1, 15)
+  name: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  @Exclude()
+  createTime: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  @Exclude()
+  updateTime: Date;
+}
