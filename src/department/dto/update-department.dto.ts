f6e73b34a061b9af726ae714fe6a58ccae76c440
zhongzhaoli <525966315@qq.com>
2024-04-15 22:58:55 +0800

    feat: updateDto可改项编辑

--- src/department/dto/update-department.dto.ts
+++ src/department/dto/update-department.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateDepartmentDto } from './create-department.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) {}
+export class UpdateDepartmentDto extends PartialType(CreateDepartmentDto) {
+  @IsOptional()
+  description: string;
+}
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,5 +1,15 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional } from 'class-validator';
 export class UpdateUserDto extends PartialType(
   OmitType(CreateUserDto, ['password'] as const),
-) {}
+) {
+  @IsOptional()
+  realName: string;
+
+  @IsOptional()
+  phone: string;
+
+  @IsOptional()
+  email: string;
+}
